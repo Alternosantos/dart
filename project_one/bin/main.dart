ab34d9dfe7c3270495cf2e8ca03b8d9aa6e1f7a6
Alberto Santos <alberto1fsantos@gmail.com>
2024-01-30 23:11:10 +0000

    first commit

---
+++ project_one/bin/main.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+void clearConsole() {
+  // Use ANSI escape codes to clear the console
+  stdout.write('\x1B[2J\x1B[0;0H');
+}
+
+void main()
+{
+    double? a;
+    double? b;
+    int? escolha;
+
+    do {
+    clearConsole();
+    print("***********CALCULADORA***********");
+    print("\nEscolha uma das opções abaixo para realizar a operação desejada: \n1-Somar \n2-Subtrair \n3-Multiplicar \n4-Dividir \n0-Sair");
+    escolha = int.parse(stdin.readLineSync()!);
+    switch (escolha) {
+        case 1:
+            print("Introduza o primeiro num:\n>> ");
+            a = double.parse(stdin.readLineSync()!);
+            print("Introduza o segundo num:\n>> ");
+            b = double.parse(stdin.readLineSync()!);
+            print("\nSoma: ${a + b}");
+            break;
+        case 4:
+            print("Introduza o primeiro num:\n>> ");
+            a = double.parse(stdin.readLineSync()!);
+            print("Introduza o segundo num:\n>> ");
+            b = double.parse(stdin.readLineSync()!);
+            if (b == 0) {
+                print("Nao pode dividir por zero!");
+            } else {
+                print("\nDivisao: ${a / b}");
+            }
+            break;
+        case 2:
+            print("Introduza o primeiro num:\n>> ");
+            a = double.parse(stdin.readLineSync()!);
+            print("Introduza o segundo num:\n>> ");
+            b = double.parse(stdin.readLineSync()!);        
+            print("\nSubtracao: ${a - b}");
+            break;
+        case  3:
+            print("Introduza o primeiro num:\n>> ");
+            a = double.parse(stdin.readLineSync()!);
+            print("Introduza o segundo num:\n>> ");
+            b = double.parse(stdin.readLineSync()!);
+            print("\nMultiplicacao: ${a * b}");
+            break;
+        case 0:
+            print("A sair...");
+            break;    
+        default:
+            print("Opcao invalida!");
+            break;
+    }
+    
+    
+    stdin.readLineSync();
+      
+    } while (escolha != 0);
+    
+}
